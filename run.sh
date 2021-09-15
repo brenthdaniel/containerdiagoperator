e5ac19f84113614d20cfc052a65ec5ee4050ab32
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2021-09-15 10:56:17 -0700

    Update run script

--- run.sh
+++ run.sh
@@ -1,11 +1,34 @@
 #!/bin/sh
+set -e
+
+TARGETCONTAINER="${TARGETCONTAINER}"
+TARGETNAMESPACE="${TARGETNAMESPACE}"
+
+if [ "${TARGETCONTAINER}" = "" ]; then
+  /bin/echo -n "Target container name: "
+  read TARGETCONTAINER
+  if [ "${TARGETCONTAINER}" = "" ]; then
+    echo "Target container name required."
+    exit 1
+  fi
+fi
+
+if [ "${TARGETNAMESPACE}" = "" ]; then
+  /bin/echo -n "Target container's namespace': "
+  read TARGETNAMESPACE
+  if [ "${TARGETNAMESPACE}" = "" ]; then
+    echo "Target container's namespace required."
+    exit 1
+  fi
+fi
+
 make undeploy
 make docker-build docker-push IMG="kgibm/containerdiagoperator:$(awk '/const OperatorVersion/ { gsub(/"/, ""); print $NF; }' controllers/containerdiagnostic_controller.go)" && \
   make deploy IMG="kgibm/containerdiagoperator:$(awk '/const OperatorVersion/ { gsub(/"/, ""); print $NF; }' controllers/containerdiagnostic_controller.go)" && \
   kubectl get pods --namespace=containerdiagoperator-system && \
   sleep 30 && \
   kubectl get pods --namespace=containerdiagoperator-system && \
-  printf '{"apiVersion": "diagnostic.ibm.com/v1", "kind": "ContainerDiagnostic", "metadata": {"name": "%s", "namespace": "%s"}, "spec": {"command": "%s", "arguments": %s, "targetObjects": %s, "steps": %s}}' diag1 containerdiagoperator-system script '[]' '[{"kind": "Pod", "name": "liberty1-774c5fccc6-f7mjt", "namespace": "testns1"}]' '[]' | kubectl create -f - && \
+  printf '{"apiVersion": "diagnostic.ibm.com/v1", "kind": "ContainerDiagnostic", "metadata": {"name": "%s", "namespace": "%s"}, "spec": {"command": "%s", "arguments": %s, "targetObjects": %s, "steps": %s}}' diag1 containerdiagoperator-system script '[]' "$(printf '[{"kind": "Pod", "name": "%s", "namespace": "%s"}]' "${TARGETCONTAINER}" "${TARGETNAMESPACE}")" '[]' | kubectl create -f - && \
   sleep 10 && \
   kubectl get ContainerDiagnostic diag1 --namespace=containerdiagoperator-system && \
   kubectl describe ContainerDiagnostic diag1 --namespace=containerdiagoperator-system && \
